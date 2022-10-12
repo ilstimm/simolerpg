eff55416a95632a4c8102e0b8b8fd91023f47339
ilstim <a20973016@gmail.com>
2022-10-12 15:02:14 +0800

    init commit

---
+++ BMICalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double weight;
+        double height;
+        double bmi;
+        String s;
+        System.out.println("Welcome to the BMI (Body Mass Index) calculator");
+        System.out.println("Please enter your weight (kilograms): ");
+        weight = input.nextDouble();
+        System.out.println("Please enter your height (centimeters): ");
+        height = input.nextDouble();
+        bmi = weight / ((height * height) / 10000);
+
+        if(bmi < 18.5)
+            s = "Underweight";
+        else if(bmi >= 18.5 && bmi < 25)
+            s = "Normal";
+        else if(bmi >= 25 && bmi < 30)
+            s = "Overweight";
+        else
+            s = "obese";
+        System.out.printf("Your BMI is %.1f (%s)", bmi, s );
+        input.close();
+    }
+}
